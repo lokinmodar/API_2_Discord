dea0b6d3660a275f7bf06b5a9d3d90cda9fa2193
Dante Souza e Souza <lokinmodar@gmail.com>
2019-11-15 13:20:53 -0300

    Update DataPost.java
    Hid Api sensitive info

--- app/src/main/java/br/com/theoldpinkeye/api2discord/remote/DataPost.java
+++ app/src/main/java/br/com/theoldpinkeye/api2discord/remote/DataPost.java
@@ -12,6 +12,6 @@ public interface DataPost {
 
     // Over here we are sending a POST request with two fields as POST request body params
     //@Headers("Content-Type:application/json")
-    @POST("api/webhooks/644907534997520385/vGm55M5EPrw_a1NOBWqstbLl0pfaMX0Ap0SrBksEy3xVLDzti26sKPTCAntoxZlZfi_y")
+    @POST("api/webhooks/[channel.id]/[webhook.id]")
     Call<ResponseBody> postDropInfo(@Body DropJson dropJson);
 }
